b25575da657f9585eae6404b63915c734eb21857
parhamsazadar <p.sazdar@gmail.com>
2021-01-25 12:32:57 +0330

    initial project

---
+++ myblog/local_settings_sample.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'SECRET_KEY'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DATABASENAME',
+        'USER': 'USERNAME',
+        'PASSWORD': 'PASSWORD',
+        'HOST': 'HOST'
+    }
+}
