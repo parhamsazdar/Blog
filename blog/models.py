746616075d2e318adfcf890a94eee5cabcfcbd8e
parhamsazadar <p.sazdar@gmail.com>
2021-01-27 09:50:30 +0330

    Improve my models

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 from django.utils import timezone
 
@@ -12,13 +11,11 @@ class Post(models.Model):
     image = models.ImageField(verbose_name='عکس پست')
     confirm = models.BooleanField('تایید')
     active = models.BooleanField('فعال')
-    like = models.IntegerField()
-    dislike = models.IntegerField()
-    date_pub=models.DateTimeField(default=timezone.now)
+    date_pub = models.DateTimeField(default=timezone.now)
     category = models.ManyToManyField('Category', through='Post_category', through_fields=('post', 'category'))
     tags = models.ManyToManyField('Tags', through='Post_tags', through_fields=('post', 'tags'))
     writer = models.ForeignKey('Writer', on_delete=models.PROTECT, related_name='posts', verbose_name='نویسنده')
-    comments=models.ManyToManyField('Comments',through='Post_comments', through_fields=('post', 'comments'))
+    comments = models.ManyToManyField('Comments', through='Post_comments', through_fields=('post', 'comments'))
 
     def __str__(self):
         return self.title
@@ -33,6 +30,14 @@ class Category(models.Model):
         return self.name
 
 
+class Like(models.Model):
+    like = models.IntegerField()
+    dislike = models.IntegerField()
+    user_id = models.ForeignKey(User, verbose_name='صاحب علاقه مندی', on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return self.user_id.first_name+" likes: "+str(self.like)+" dislike: "+str(self.dislike)
+
 class Tags(models.Model):
     name = models.CharField('پرجسب', max_length=30)
 
@@ -41,21 +46,21 @@ class Tags(models.Model):
 
 
 class Writer(models.Model):
-    first_name = models.CharField("نام", max_length=10)
-    last_name = models.CharField("نام خانوادگی", max_length=10)
-    email = models.EmailField("ایمیل")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='آِِِیدی کاربری', related_name='wirter')
     photo = models.ImageField(verbose_name='عکس کاربر')
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='آیدی کاربری', related_name='writer')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='آیدی کاربری', related_name='writer',null=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True, related_name='writer_post')
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
+
 class Comments(models.Model):
-    title=models.CharField('عنوان',max_length=20)
-    text=models.TextField('متن')
+    title = models.CharField('عنوان', max_length=20)
+    text = models.TextField('متن')
     confirm = models.BooleanField('تایید')
     date_pub = models.DateTimeField(default=timezone.now)
+    user_id = models.ForeignKey(User, verbose_name='صاحب نظر', related_name='comment', on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return self.title
@@ -80,7 +85,9 @@ class Post_tags(models.Model):
 
 
 class Post_comments(models.Model):
-    post=models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments', verbose_name='پست', null=True,
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post_comments', verbose_name='پست',
+                             null=True,
                              blank=True)
-    comments = models.ForeignKey(Comments, on_delete=models.CASCADE, related_name='comment', verbose_name='نظر', null=True,
-                             blank=True)
+    comments = models.ForeignKey(Comments, on_delete=models.CASCADE, related_name='comment', verbose_name='نظر',
+                                 null=True,
+                                 blank=True)
