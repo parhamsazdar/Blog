45b8caa475b826a6580f68f58b40ae59114ff4ea
parhamsazadar <p.sazdar@gmail.com>
2021-02-18 01:33:28 +0330

    cleane my html code

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('cat_post/<int:cat_id>', views.suc_category, name='suc_category'),
     path('writer_post/<int:writer_id>', views.writer_post, name="writer_post"),
     path('post_tag/<int:tag_id>', views.post_tag, name="post_tag"),
-    path('log_out/',views.log_out,name='log_out')
+    path('log_out/', views.log_out, name='log_out'),
+    path('latest_post/', views.LatestPost.as_view(), name="LatestPost"),
+    path('popular_post/', views.PopularPost.as_view(), name="PopularPost")
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseForbidden
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+
 from .user_func import *
 from blog.forms import Comment
 from blog.models import Post, Category, Tags
@@ -20,20 +22,23 @@ def index(request):
 def writer_post(request, writer_id):
     post = Post.objects.filter(user__pk=writer_id).order_by("-date_pub")
     writer = User.objects.filter(pk=writer_id)
-    return render(request, 'blog/writer_post.html', {"posts": post, "writer": writer})
+    header=f"جدیدترین پست های {writer[0].first_name+' '+ writer[0].last_name}"
+    return render(request, 'blog/writer_post.html', {"posts": post, "header": header})
 
 
 def post_tag(request, tag_id):
     tag = Tags.objects.filter(pk=tag_id)
     post = Post.objects.filter(tags__in=tag).order_by('-date_pub')
-    return render(request, 'blog/tag_post.html', {"posts": post, "tag": tag})
+    header=f"جدیدترین پست های بر چسب {tag[0].name}"
+    print(header)
+    return render(request, 'blog/tag_post.html', {"posts": post, "header": header})
 
 
 def suc_category(request, cat_id):
     post_of_cat = Post.objects.filter(category__id=cat_id).order_by('-date_pub')
     category = Category.objects.filter(pk=cat_id)
-
-    return render(request, "blog/subcategory.html", {"post_of_cat": post_of_cat, "category": category})
+    header=f"جدیدترین پست های {category[0].name}"
+    return render(request, "blog/subcategory.html", {"post_of_cat": post_of_cat, "category":category,"header": header})
 
 
 def post_show(request, post_id):
@@ -46,11 +51,33 @@ def post_show(request, post_id):
         return HttpResponseForbidden("این پست از دسترس خارج است")
 
 
+class LatestPost(generic.ListView):
+    template_name = 'blog/latest_post.html'
+    context_object_name = "latest_post"
+
+    def get_context_data(self, **kwargs):
+        context = super(LatestPost, self).get_context_data(**kwargs)
+        context['header'] = "جدیدترین پست ها"
+        return context
+
+    def get_queryset(self):
+        return Post.objects.filter(active=True, confirm=True).order_by('-date_pub')
+
+class PopularPost(generic.ListView):
+    template_name = 'blog/popular_post.html'
+    context_object_name = "popular_post"
+
+    def get_context_data(self, **kwargs):
+        context = super(PopularPost, self).get_context_data(**kwargs)
+        context['header'] = "محبوب ترین پست ها"
+        return context
+
+    def get_queryset(self):
+        return Post.objects.annotate(like_count=Count('like')).order_by('-like_count')
+
 def log_out(request):
     logout(request)
     # messages.success(request, 'خروج با موفقیت انجام شد')
     return redirect(request.META.get('HTTP_REFERER'))
 
 
-# def record_like(request):
-#     if request.POST:
